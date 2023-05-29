609425ea96e54756eb33e7b915f10641df86a474
rabib-jahin <rabibjahin2000@gmail.com>
2023-05-29 13:17:48 +0600

    task-4 partially done,created book related apis

---
+++ Backend/src/main/java/com/vinternship/rabib/app/service/BookService.java
@@ -0,0 +1,69 @@
+package com.vinternship.rabib.app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vinternship.rabib.app.model.Book;
+import com.vinternship.rabib.app.repository.BookRepository;
+
+@Service
+public class BookService {
+	
+	@Autowired
+	private BookRepository repository;
+    public Book saveBook(Book book){
+		
+		
+		
+		return repository.save(book);
+}
+ public Book updateBook(Book book,long id){
+		
+		
+		book.setBookId(id);
+		
+		
+		return repository.save(book);
+	}
+	
+	public List<Book> getBooks(){
+		
+		
+		
+		return repository.findAll();
+	}
+	
+public Book getBookById(long id){
+		
+		
+		
+		return repository.findById(id).orElse(null);
+	}
+public List<Book> getBookByTitle(String title){
+	
+	
+	
+	return repository.findByTitle(title);
+}
+
+public void delBookById(long id){
+	
+
+	
+	
+	 repository.deleteById(id);
+	 
+	 
+}
+public void delBooks(){
+	
+
+	
+	
+	 repository.deleteAll();
+	 
+	 
+}
+}
